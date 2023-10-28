9a53d2821d3e6022a24cbda3a66306ab41ad39c8
Vijay Dahiphale <vijaydahiphale96@gmail.com>
2023-10-27 22:34:39 -0400

    added optimization

--- ferryLoading.cpp
+++ ferryLoading.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
+#include <utility>
 #include <algorithm>
 
 using namespace std;
 
+// Custom hash function for std::pair<int, int>
+struct PairHash {
+    template <class T1, class T2>
+    std::size_t operator () (const std::pair<T1, T2>& p) const {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+        return h1 ^ h2;
+    }
+};
+
+// Custom equality comparison for std::pair<int, int>
+struct PairEqual {
+    template <class T1, class T2>
+    bool operator () (const std::pair<T1, T2>& p1, const std::pair<T1, T2>& p2) const {
+        return p1.first == p2.first && p1.second == p2.second;
+    }
+};
+
 int maxCarsAllocation(int n, vector<int>& carLengths, int ferryLength) {
     // vector<vector<vector<int>> > dp(n + 1, vector<vector<int>>(ferryLength+1, vector<int>(2, 0)));
+    unordered_set<std::pair<int, int>, PairHash, PairEqual> calculatedMatrixPoints;
     vector<vector<vector<int>>> dp(n+1);
     vector<int> v1 = {0,0};
     dp[0].push_back(v1);
@@ -17,13 +38,21 @@ int maxCarsAllocation(int n, vector<int>& carLengths, int ferryLength) {
             int rightLineSize = dp[i-1][j][1];
 
             if(carLength <= (ferryLength - leftLineSize)) {
-                vector<int> v2 = {leftLineSize + carLength, rightLineSize};
-                dp[i].push_back(v2);
+                pair<int, int> myPair(leftLineSize + carLength, rightLineSize);
+                if(calculatedMatrixPoints.find(myPair) == calculatedMatrixPoints.end()) {
+                    calculatedMatrixPoints.insert(myPair);
+                    vector<int> v2 = {leftLineSize + carLength, rightLineSize};
+                    dp[i].push_back(v2);
+                }
             }
 
             if(carLength <= (ferryLength - rightLineSize)) {
-                vector<int> v2 = {leftLineSize, rightLineSize + carLength};
-                dp[i].push_back(v2);
+                pair<int, int> myPair(leftLineSize, rightLineSize + carLength);
+                if(calculatedMatrixPoints.find(myPair) == calculatedMatrixPoints.end()) {
+                    calculatedMatrixPoints.insert(myPair);
+                    vector<int> v2 = {leftLineSize, rightLineSize + carLength};
+                    dp[i].push_back(v2);
+                }
             }
             
             if(carLength > (ferryLength - leftLineSize) && carLength > (ferryLength - rightLineSize)) {
